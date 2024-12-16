de7a6ed4c4449e2eb035de7560b4c68ea27b0a3c
Thomas Chobert <tchobert@made-f0Br7s16.clusters.42paris.fr>
2024-12-16 19:58:37 +0100

    refacto create token

--- srcs/syntax_analysis/lexing/create_token.c
+++ srcs/syntax_analysis/lexing/create_token.c
@@ -12,20 +12,28 @@
 
 #include "minishell.h"
 
+static t_token	*initialize_token_content(t_token *new_token,
+					char *new_token_content, t_token_type new_token_type)
+{
+	new_token->token_content = ft_strdup(new_token_content);
+	if (new_token->token_content == NULL)
+	{
+		free(new_token);
+		return (NULL);
+	}
+	new_token->token_type = new_token_type;
+	new_token->previous_token = NULL;
+	new_token->next_token = NULL;
+	return (new_token);
+}
+
 t_token	*create_token(char *new_token_content, t_token_type new_token_type)
 {
-	(void)new_token_content;
-	(void)new_token_type;
-
 	t_token	*new_token;
 
 	new_token = (t_token *)malloc(sizeof(t_token));
-	if (new_token != NULL)
-	{
-		new_token->token_content = ft_strdup(new_token_content);
-		new_token->token_type = new_token_type;
-		new_token->previous_token = NULL;
-		new_token->next_token = NULL;
-	}
-	return (new_token);
+	if (new_token == NULL)
+		return (NULL);
+	return (initialize_token_content(new_token, new_token_content,
+					new_token_type));
 }
