24ec80e539cfc09cec6e557b466a1451c4f888e6
Thomas Chobert <tchobert@MADE-f0Dr8s2.clusters.42paris.fr>
2024-12-17 15:57:23 +0100

    print token list

--- tests/feature_syntax_analysis/lexing/unit_tests/delete_token_list_test.c
+++ tests/feature_syntax_analysis/lexing/unit_tests/delete_token_list_test.c
@@ -12,6 +12,34 @@
 
 #include "tests.h"
 
+void	test_delete_token_list(void)
+{
+	printf("Testing lexing: delete_token_list\n");
+
+	//ARRANGE
+
+	t_token_list	token_list_test;
+	ft_bzero(&token_list_test, sizeof(t_token_list));
+	t_token			*token_test_1 = create_token("test_1", TOKEN_LIST_START);
+	t_token			*token_test_2 = create_token("test 2", WORD);
+	t_token			*token_test_3 = create_token("test 3", TOKEN_LIST_END);
+
+	add_token_to_token_list(&token_list_test, token_test_1);
+	add_token_to_token_list(&token_list_test, token_test_2);
+	add_token_to_token_list(&token_list_test, token_test_3);
+
+	print_token_list(token_list_test);
+	//ACT
+
+	delete_token_list(&token_list_test);
+
+	//ASSERT
+
+	TEST_ASSERT_EQUAL(NULL, token_list_test.token_list_first_token);
+	TEST_ASSERT_EQUAL(NULL, token_list_test.token_list_last_token);
+	TEST_ASSERT_EQUAL(NULL, token_list_test.token_list_first_token);
+}
+
 /*
 
 FONCTION "DELETE TOKEN LIST"
@@ -24,5 +52,7 @@ OUTPUT: none
 PROCESS:
 
 A/ Parcourt la liste de tokens, applique sur chacun la fonction "DELETE TOKEN".
+
+B/ Une fois l'operation terminee, met la lsite a NULL.
 
 */
---
+++ tests/feature_syntax_analysis/lexing/unit_tests/print_token_list.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_token_list.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 15:43:23 by tchobert          #+#    #+#             */
+/*   Updated: 2024/12/17 15:43:35 by tchobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tests.h"
+
+void	print_token_list(t_token_list token_list)
+{
+	const char	*enum_names[] = {"TOKEN_LIST_START", "TOKEN_LIST_END","WORD", "PIPE", "INPUT_REDIR_OPERATOR", "OUTPUT_REDIR_OPERATOR", "APPEND_OPERATOR", "HEREDOC OPERATOR", "UNKNOWN OPERATOR"};
+	t_token	*current_token = token_list.token_list_first_token;
+
+	while(current_token != token_list.token_list_last_token)
+	{
+		printf("Content = %s\n", current_token->token_content);
+		printf("Token type = %s\n", enum_names[current_token->token_type]);
+		printf("\n");
+		current_token = current_token->next_token;
+	}
+}
