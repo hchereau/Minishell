4352866a5b3d9348a1a22116d4f064b2ec22e632
hchereau <hchereau42@gmail.com>
2024-12-22 13:07:43 +0100

    tests

--- includes/environment.h
+++ includes/environment.h
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   environment.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 16:11:56 by hucherea          #+#    #+#             */
-/*   Updated: 2024/12/22 13:04:00 by linux            ###   ########.fr       */
+/*   Created: 2024/12/22 13:03:41 by linux             #+#    #+#             */
+/*   Updated: 2024/12/22 13:03:53 by linux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef ENVIRONMENT_H
+# define ENVIRONMENT_H
 
-// INCLUDES
 
-# include "history.h"
-# include "libft.h"
-# include "user_interface.h"
-# include "parsing.h"
-# include "environment.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-
-// DEFINES
-
-# define MSH_LOOP 1
-
-// PROTOTYPES
-
-int		exit_shell_routine(void);
-char	*prompt_gets_user_input(void);
-
-#endif
+# endif
--- tests/environment/env_builtin.test.c
+++ tests/environment/env_builtin.test.c
@@ -1,36 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   env_builtin.test.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 16:11:56 by hucherea          #+#    #+#             */
-/*   Updated: 2024/12/22 13:04:00 by linux            ###   ########.fr       */
+/*   Created: 2024/12/22 13:03:06 by linux             #+#    #+#             */
+/*   Updated: 2024/12/22 13:03:18 by linux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-// INCLUDES
-
-# include "history.h"
-# include "libft.h"
-# include "user_interface.h"
-# include "parsing.h"
-# include "environment.h"
-
-# include <stdio.h>
-# include <stdlib.h>
-
-// DEFINES
-
-# define MSH_LOOP 1
-
-// PROTOTYPES
-
-int		exit_shell_routine(void);
-char	*prompt_gets_user_input(void);
-
-#endif
+#include "minishell.h"
